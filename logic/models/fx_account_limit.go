1ad5ca2e720ebfb4c570b480e2f94a4656e0f3ca
zhoulindong <zhoulindong@qima-inc.com>
2016-11-14 14:15:54 +0800

    add file

---
+++ logic/models/fx_account_limit.go
@@ -0,0 +1,35 @@
+package models
+
+import (
+	"fmt"
+	"time"
+	
+	"github.com/Sirupsen/logrus"
+)
+
+type FxAccountLimit struct {
+	ID              int64  `xorm:"pk autoincr"`
+	UnionId         string `xorm:"not null default '' varchar(128) unique"`
+	CheckIn         int64  `xorm:"not null default 0 int"`
+	WithdrawalTimes int64  `xorm:"not null default 0 int"`
+	CreatedAt       int64  `xorm:"not null default 0 int"`
+	UpdatedAt       int64  `xorm:"not null default 0 int"`
+}
+
+func CreateFxAccountLimt(info *FxAccountLimit) error {
+	if info.UnionId == "" {
+		return fmt.Errorf("fx account limit union_id[%s] cannot be nil.", info.UnionId)
+	}
+	
+	now := time.Now().Unix()
+	info.CreatedAt = now
+	info.UpdatedAt = now
+	_, err := x.Insert(info)
+	if err != nil {
+		logrus.Errorf("create fx account limit error: %v", err)
+		return err
+	}
+	logrus.Infof("fx account limit union_id[%s] create success.", info.UnionId)
+	
+	return nil
+}
