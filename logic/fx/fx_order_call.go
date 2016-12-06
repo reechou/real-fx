a3e9c6a3bab58f98950562705516e41db567ae8d
zhoulindong <zhoulindong@qima-inc.com>
2016-12-06 15:40:01 +0800

    fix

--- logic/fx/fx_order_call.go
+++ logic/fx/fx_order_call.go
@@ -22,6 +22,9 @@ func (fxr *FXRouter) createFxOrder(ctx context.Context, w http.ResponseWriter, r
 	}
 
 	rsp := &FxResponse{Code: RspCodeOK}
+	rsp.Msg = "暂时无法提交"
+	rsp.Code = RspCodeErr
+	return utils.WriteJSON(w, http.StatusOK, rsp)
 
 	fxAccount := &models.FxAccount{
 		UnionId: req.UnionId,
